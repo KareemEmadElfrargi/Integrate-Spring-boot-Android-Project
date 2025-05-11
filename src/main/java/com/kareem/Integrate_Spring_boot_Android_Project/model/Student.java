b2fccc861e284053a9bb46bf1f352ddf77737c3a
Kareem Emad <@gmail.com>
2025-05-11 12:28:46 +0300

    Initial commit

---
+++ src/main/java/com/kareem/Integrate_Spring_boot_Android_Project/model/Student.java
@@ -0,0 +1,4 @@
+package com.kareem.Integrate_Spring_boot_Android_Project.model;
+
+public class Student {
+}
---
+++ src/test/java/com/kareem/Integrate_Spring_boot_Android_Project/IntegrateSpringBootAndroidProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kareem.Integrate_Spring_boot_Android_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrateSpringBootAndroidProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
